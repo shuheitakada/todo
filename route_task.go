3088e6601b5b4ce941925ae5136edd73e4dd31fc
shuheitakada <s-takada@giftee.co>
2020-10-13 23:26:55 +0900

    task.goをroute_task.goに改名

--- route_task.go
+++ route_task.go
@@ -8,8 +8,18 @@ import (
 func handleTasks(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"templates/layout.html",
-		"templates/tasks/index.html",
 		"templates/navbar.html",
+		"templates/tasks/index.html",
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", nil)
+}
+
+func handleNewTask(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"templates/layout.html",
+		"templates/navbar.html",
+		"templates/tasks/new.html",
 	}
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", nil)
