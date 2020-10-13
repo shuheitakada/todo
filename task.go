11ec0381bef7592aa10ae3639b20d319d781d864
shuheitakada <s-takada@giftee.co>
2020-10-13 22:53:29 +0900

    navbar作成とbootstrap導入

--- task.go
+++ task.go
@@ -9,6 +9,7 @@ func handleTasks(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"templates/layout.html",
 		"templates/tasks/index.html",
+		"templates/navbar.html",
 	}
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", nil)
