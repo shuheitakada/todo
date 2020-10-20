4c8ae51e812f2f81177ca4a23e347d237815f1fc
shuheitakada <s-takada@giftee.co>
2020-10-20 15:29:58 +0900

    Delete: 使わなくなったdatabase/sqlを削除

--- data/data.go
+++ data/data.go
@@ -1,25 +1,18 @@
 package data
 
 import (
-	"database/sql"
 	"log"
 
-	_ "github.com/lib/pq"
 	"gorm.io/gorm"
 	"gorm.io/driver/postgres"
 )
 
-var Db *sql.DB
 var DbGorm *gorm.DB
 
 func init() {
 	var err error
 	dsn := "dbname=todo sslmode=disable"
 	DbGorm, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	Db, err = sql.Open("postgres", "dbname=todo sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
