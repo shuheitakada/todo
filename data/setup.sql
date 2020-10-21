b1c1b0c0b7ca0657d4594f2e83d6c8b66e7075f1
shuheitakada <s-takada@giftee.co>
2020-10-21 17:21:19 +0900

    Add: usersテーブルを作成

--- data/setup.sql
+++ data/setup.sql
@@ -1,9 +1,20 @@
 drop table tasks;
+drop table users;
 
 create table tasks (
   id          serial primary key,
   name        varchar(255) not null,
   description text,
+  created_at  timestamp not null,
+  updated_at  timestamp not null
+);
+
+create table users (
+  id serial primary key,
+  name varchar(255),
+  email varchar(255) not null,
+  crypted_password varchar(255) not null,
+  salt varchar(255) not null,
   created_at  timestamp not null,
   updated_at  timestamp not null
 );
