0809262c966616af0ab32597409045b1158ce729
shuheitakada <s-takada@giftee.co>
2020-10-21 17:38:40 +0900

    Add: taskの初期insert

--- data/setup.sql
+++ data/setup.sql
@@ -9,6 +9,10 @@ CREATE TABLE tasks (
   updated_at  timestamp NOT NULL
 );
 
+INSERT INTO tasks (name, description, created_at, updated_at) VALUES ('タスク1', 'ここにタスク1の説明が入ります', now(), now());
+INSERT INTO tasks (name, description, created_at, updated_at) VALUES ('タスク2', 'ここにタスク2の説明が入ります', now(), now());
+INSERT INTO tasks (name, description, created_at, updated_at) VALUES ('タスク3', 'ここにタスク3の説明が入ります', now(), now());
+
 CREATE TABLE users (
   id               serial PRIMARY KEY,
   name             varchar(255),
