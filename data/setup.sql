df0b9c22ac12b29eb28e52be059e3074614f6828
shuheitakada <s-takada@giftee.co>
2020-10-26 14:07:17 +0900

    Fix: setupするときのテーブル削除とテーブル作成の順番を変更。tasks.user_idを追加

--- data/setup.sql
+++ data/setup.sql
@@ -1,18 +1,6 @@
-DROP TABLE tasks;
 DROP TABLE sessions;
+DROP TABLE tasks;
 DROP TABLE users;
-
-CREATE TABLE tasks (
-  id          serial PRIMARY KEY,
-  name        varchar(255) NOT NULL,
-  description text,
-  created_at  timestamp NOT NULL,
-  updated_at  timestamp NOT NULL
-);
-
-INSERT INTO tasks (name, description, created_at, updated_at) VALUES ('タスク1', 'ここにタスク1の説明が入ります', now(), now());
-INSERT INTO tasks (name, description, created_at, updated_at) VALUES ('タスク2', 'ここにタスク2の説明が入ります', now(), now());
-INSERT INTO tasks (name, description, created_at, updated_at) VALUES ('タスク3', 'ここにタスク3の説明が入ります', now(), now());
 
 CREATE TABLE users (
   id               serial PRIMARY KEY,
@@ -22,6 +10,15 @@ CREATE TABLE users (
   salt             varchar(255) NOT NULL,
   created_at       timestamp NOT NULL,
   updated_at       timestamp NOT NULL
+);
+
+CREATE TABLE tasks (
+  id          serial PRIMARY KEY,
+  name        varchar(255) NOT NULL,
+  description text,
+  user_id     integer REFERENCES users(id),
+  created_at  timestamp NOT NULL,
+  updated_at  timestamp NOT NULL
 );
 
 CREATE TABLE sessions (
